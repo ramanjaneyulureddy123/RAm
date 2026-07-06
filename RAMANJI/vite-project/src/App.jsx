7c4b3b08e0d74dcae5191c2dd3defa3b88d56327
Ramanjaneyulu Reddy Pandillapalli <ramanjaneyulu.pandillapalli@nybinfotech.com>
2026-07-06 18:14:14 +0530

    week6 react introduction

--- RAMANJI/vite-project/src/App.jsx
+++ RAMANJI/vite-project/src/App.jsx
@@ -1,29 +1,15 @@
 import React from 'react'
-import App from './final/Product'
-// import CartApp from './15-05-2026/Cart'
+import Minip from './week6-7-2026/Minip'
+//  import Toggle from './assement/Toggle'
 
-// import Red from './20-05-2026/Red'
-// import { Provider } from 'react-redux'
-// import { store } from './20-05-2026/Reduxtopic'
-// import Hook from './hooks29-05-2026/Hook'
-// import Toggle from './assement/Toggle'
+
 function App() {
   return (
     <div>
-      {/* <Product/> */}
+    
+{/* <App/> */}
 
-      {/* <Appp/> */}
-      {/* <Eventhandling/> */}
-      {/* <Provider store={store}>
-        <Red/>
-      </Provider> */}
-{/* <Hook/> */}
-{/* <Toggle/> */}
-{/* <UseEffect/> */}
-{/* <UseContext/> */}
-<App/>
-
-      <Ass/>
+    <Minip/>
     </div>
   )
 }
---
+++ week6/06-07-2026/Mini.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function mini() {
+    
+    const name="anji";
+    const age=19;
+    const role="engineer"
+    const city="chennai"
+  return (
+    <div>
+     <h1>welcome to react application</h1>
+         <h2>Personal information</h2>
+        <p>Name: {name}</p>
+      <p>Age: {age}</p>
+      <p>Role: {role}</p>
+      <p>City: {city}</p>
+
+
+      <h2>Skills</h2>
+      <ul>
+        <li>HTML</li>
+        <li>JS</li>
+        <li>CSS</li>
+        <li>React</li>
+      </ul>
+
+      <p>This is all about my information</p>
+
+    </div>
+  )
+}
+
+export default mini
